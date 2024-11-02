a9687aaf40c97628cb362340f83c98e0b5deba3c
Mithun M <mmithunsai@gmail.com>
2024-11-02 08:56:33 +0530

    adds types

--- app.ts
+++ app.ts
@@ -1,16 +1,29 @@
+interface Item{
+    id: number,
+    title: string,
+    status: CompletionStatus,
+    completedOn?: Date
+}
+
+enum CompletionStatus{
+    Done= "done",
+    InProgress= "in progress",
+    ToDo= "todo",
+}
+
 const todoItems = [
-    { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
-    { id: 2, title: "Learn TypeScript", status: "in-progress" },
-    { id: 3, title: "Write the best app in the world", status: "todo" },
+    { id: 1, title: "Learn HTML", status: CompletionStatus.Done, completedOn: new Date("2021-09-11") },
+    { id: 2, title: "Learn TypeScript", status: CompletionStatus.InProgress },
+    { id: 3, title: "Write the best app in the world", status: CompletionStatus.ToDo },
 ]
 
-function addTodoItem(todo) {
-    const id = getNextId(todoItems)
+function addTodoItem(todo:string):Item {
+    const id:number = getNextId(todoItems)
 
-    const newTodo = {
+    const newTodo:Item = {
         id,
         title: todo,
-        status: "todo",
+        status: CompletionStatus.ToDo,
     }
 
     todoItems.push(newTodo)
@@ -18,10 +31,10 @@ function addTodoItem(todo) {
     return newTodo
 }
 
-function getNextId(items) {
-    return items.reduce((max, x) => x.id > max ? max : x.id, 0) + 1
+function getNextId(items): number {
+    return items.reduce((max:number, x:Item) => x.id > max ? max : x.id, 0) + 1
 }
 
-const newTodo = addTodoItem("Buy lots of stuff with all the money we make from the app")
+const newTodo:Item = addTodoItem("Buy lots of stuff with all the money we make from the app")
 
 console.log(JSON.stringify(newTodo))
